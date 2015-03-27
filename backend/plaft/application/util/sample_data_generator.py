805d74921b4ff8c7b86ae59d55020ae2fd2e1f06
Javier Huaman <jahanjp09@gmail.com>
2015-03-27 10:14:36 -0500

    Simple-Data-Generator

--- backend/plaft/application/util/sample_data_generator.py
+++ backend/plaft/application/util/sample_data_generator.py
@@ -12,20 +12,48 @@ from __future__ import unicode_literals
 # from plaft.domain.model import (Officer, Customs, Business, Person,
 #                                 Document, Stakeholder, Employee, Dispatch,
 #                                 CodeName)
-from plaft.domain.model import Officer, Employee, Customer, Dispatch
+from plaft.domain.model import (Officer, Employee, Dispatch, CodeName,
+                                  Customer, Third, Declarant, Linked)
 
 
-def create_customers():
-    queirolo = Customer(name='SANTIAGO QUEIROLO S.A.C',
-                        document_number='12345678989',  # 20100097746
-                        document_type='ruc')
-    queirolo.store()
+#def create_customers():
 
-    gcca = Customer(
-        name='cristHian Gz. (gcca)',
-        document_number='12345678',  # 20100097746
-        document_type='dni')
-    gcca.store()
+#    queirolo = Business(
+#        name='SANTIAGO QUEIROLO S.A.C',
+#        document_number='12345678989',
+#        document_type='ruc',  # 20100097746
+#        birthday='25/01/1960',
+#        social_object='ELABORACION DE VINOS',
+#        activity='IMPORTADOR/EXPORTADOR',
+#        address=('AV. AVENIDA SAN MARTIN #1062 '
+#                 'LIMA / LIMA / PUEBLO LIBRE (MAGDALENA VIEJA)'),
+#        phone='4631008, 4636503, 4638777, 4616552, 4631008, 4636503',
+#        shareholders=[
+#            Business.Shareholder(name='Fidel de Santa Cruz',
+#                                 document_type='dni',
+#                                 document_number='45678989',
+#                                 ratio='4'),
+#            Business.Shareholder(name='Artemisa Kalonice',
+#                                 document_type='dni',
+#                                 document_number='69657894',
+#                                 ratio='7')
+#        ],
+#        is_obligated = "Si",
+#        has_officer = "No",
+#        third = Third(identification_type='Si',
+#                third_type='Juridico',
+#                third_name='IEI Municipal'))
+#    queirolo.store()
+
+#    gcca = Person(
+#        name='cristHian Gz. (gcca)',
+#        document_number='12345678',
+#        document_type='dni',  # 20100097746
+#        birthday='06/06/2006',
+#        activity='Ingeniero',
+#        address=('Atrás del mar'),
+#        phone='555 5555')
+#    gcca.store()
 
 
 def users():
@@ -61,16 +89,95 @@ def users():
     cava.store()
 
     # Customers ############################################################
-    create_customers()
+    queirolo = Customer(
+        name='SANTIAGO QUEIROLO S.A.C',
+        document_number='12345678989',
+        document_type='ruc',  # 20100097746
+        birthday='25/01/1960',
+        social_object='ELABORACION DE VINOS',
+        activity='IMPORTADOR/EXPORTADOR',
+        address=('AV. AVENIDA SAN MARTIN #1062 '
+                 'LIMA / LIMA / PUEBLO LIBRE (MAGDALENA VIEJA)'),
+        phone='4631008, 4636503, 4638777, 4616552, 4631008, 4636503',
+        shareholders=[
+            Customer.Shareholder(name='Fidel de Santa Cruz',
+                                 document_type='dni',
+                                 document_number='45678989',
+                                 ratio='4'),
+            Customer.Shareholder(name='Artemisa Kalonice',
+                                 document_type='dni',
+                                 document_number='69657894',
+                                 ratio='7')
+        ],
+        is_obligated = "Si",
+        has_officer = "No",
+        third = Third(identification_type='Si',
+                third_type='Juridico',
+                name='IEI Municipal'))
+    queirolo.store()
+
+    gcca = Customer(
+        name='cristHian Gz. (gcca)',
+        document_number='12345678',
+        document_type='dni',  # 20100097746
+        birthday='06/06/2006',
+        activity='Ingeniero',
+        address=('Atrás del mar'),
+        phone='555 5555')
+    gcca.store()
+
+
+    # Linkeds ##############################################################
+    lnk1 = Linked(name='Manolete',
+         social_object='MANOA',
+         customer_type= 'Juridica')
+    lnk1.store()
+    lnk2 = Linked(name='Atlas',
+                document_type='dni',
+                document_number='69657894',
+                father_name= 'NI idae',
+                mother_name= 'Elberto',
+                customer_type= 'Natural')
+    lnk2.store()
+
+    # Declarants ###########################################################
+
+    dcl1 = Declarant(name='Manolete',
+             document_type='dni',
+             document_number='45678989',
+             father_name = 'Sebastian',
+             mother_name = 'MOndragon')
+    dcl1.store()
+    dcl2 = Declarant(name='Atlas',
+             document_type='dni',
+             document_number='69657894',
+             father_name= 'NI idae',
+             mother_name= 'Elberto')
+    dcl2.store()
 
     # Dispatches ###########################################################
 
-    disp1 = Dispatch(order='2014-601')
+    disp1 = Dispatch(reference='Debe haber una referencia!!!',
+                     order='2014-601',
+#                     customer=queirolo.key,
+                     declaration=queirolo,
+                     jurisdiction=CodeName(code='947',
+                                           name='AEROPUERTO DE TACNA'),
+                     regime=CodeName(code='10',
+                                     name='Importacion para el Consumo'),
+                     declarant=[dcl1,dcl2],
+                     linked=[lnk1,lnk2],
+                     description='Debe haber una descripcion !!!!',
+                     income_date='20/12/1996')
     disp1.store()
-
-    disp2 = Dispatch(order='2014-604')
+    disp2 = Dispatch(order='2014-604',
+                     declaration=gcca,
+                     jurisdiction=CodeName(code='9',
+                                        name='AEROPUERTO CALLAO'),
+                     dam='2014-103-8237',
+                     amount='56 874',
+                     canal='V')
     disp2.store()
-
     ########################################################################
     return
     ########################################################################
@@ -84,37 +191,6 @@ def users():
 
     choice = Customs(name='Choice')
     choice.store()
-
-
-    queirolo = Business(
-        name='SANTIAGO QUEIROLO S.A.C',
-        document=Document(type='ruc', number='12345678989'),  # 20100097746
-        birthday='25/01/1960',
-        social_object='ELABORACION DE VINOS',
-        activity='IMPORTADOR/EXPORTADOR',
-        address=('AV. AVENIDA SAN MARTIN #1062 '
-                 'LIMA / LIMA / PUEBLO LIBRE (MAGDALENA VIEJA)'),
-        phone='4631008, 4636503, 4638777, 4616552, 4631008, 4636503',
-        shareholders=[
-            Business.Shareholder(name='Fidel de Santa Cruz',
-                                 document=Document(type='dni',
-                                                   number='45678989'),
-                                 ratio='4'),
-            Business.Shareholder(name='Artemisa Kalonice',
-                                 document=Document(type='dni',
-                                                   number='69657894'),
-                                 ratio='8')
-        ])
-    queirolo.store()
-
-    gcca = Person(
-        name='cristHian Gz. (gcca)',
-        document=Document(type='dni', number='12345678'),  # 20100097746
-        birthday='06/06/2006',
-        activity='Ingeniero',
-        address=('Atrás del mar'),
-        phone='555 5555')
-    gcca.store()
 
 
     metro = Stakeholder(name='Hipermercados Metro',
