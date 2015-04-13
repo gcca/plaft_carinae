9ec4217a18923f8f2fa1adb1b307a246f33cb663
Javier Huaman <jahanjp09@gmail.com>
2015-04-13 14:32:03 -0500

    Uni test: application - dispatch: register, create_without_customer, pending, numerate.

--- resources/test/test_application_dispatch.py
+++ resources/test/test_application_dispatch.py
@@ -1,6 +1,7 @@
 import testplaft
 import plaft.application.dispatch
-from plaft.domain.model import Dispatch, Customer, CustomsAgency, Datastore
+from plaft.domain.model import Dispatch, Customer, CustomsAgency, \
+                               Datastore, Declaration
 
 
 class ApplicationDispatchTest(testplaft.TestCase):
@@ -20,8 +21,8 @@ class ApplicationDispatchTest(testplaft.TestCase):
         dispatch = Dispatch(order='666-666')
 
         plaft.application.dispatch.create(dispatch,
-                                          customer,
-                                          customs_agency)
+                                          customs_agency,
+                                          customer)
 
         # test creation
         dispatch_test = Dispatch.find(order='666-666')
@@ -34,6 +35,83 @@ class ApplicationDispatchTest(testplaft.TestCase):
         customs_agency.delete()
         datastore.delete()
         customer.delete()
+        dispatch.delete()
+
+    def test_create_without_customer(self):
+        # premises
+        customs_agency = CustomsAgency()
+        customs_agency.store()
+
+        datastore = Datastore(customs_agency=customs_agency.key)
+        datastore.store()
+
+        # use case
+        customer = Customer(document_number='123123123')
+        declaration = Declaration(customer=customer)
+        declaration.store()
+        dispatch = Dispatch(order='666-666', declaration=declaration.key)
+
+        plaft.application.dispatch.create(dispatch, customs_agency)
+
+        # test creation
+        customer = Customer.find(document_number='123123123')
+        self.assertIsNotNone(customer)
+
+    def test_numerate(self):
+        # premises
+        dispatch = Dispatch(order='123-123')
+        dispatch.store()
+        # use case
+        plaft.application.dispatch.numerate(dispatch, dam='201561-12')
+
+        # test
+        dispatch_test = Dispatch.find(order='123-123')
+        self.assertEqual(dispatch_test.dam, '201561-12')
+
+        dispatch.delete()
+
+    def test_pending(self):
+        # premises
+        customs_agency = CustomsAgency()
+        customs_agency.store()
+
+        dispatch1 = Dispatch(order='123-123')
+        dispatch1.customs_agency = customs_agency.key
+        dispatch1.store()
+
+        dispatch2 = Dispatch(order='345-345')
+        dispatch2.customs_agency = customs_agency.key
+        dispatch2.store()
+
+        # use case
+        dispatches = plaft.application.dispatch.pending(customs_agency)
+
+        # test
+        self.assertListEqual([dispatch1, dispatch2], dispatches)
+
+        dispatch1.delete()
+        dispatch2.delete()
+        customs_agency.delete()
+
+    def test_register(self):
+        # premises
+        dispatch = Dispatch(order='123-123')
+        dispatch.store()
+
+        country_source = 'PERU'
+        country_target = 'CHILE'
+
+        # use case
+        plaft.application.dispatch.register(dispatch,
+                                            country_source,
+                                            country_target)
+
+        # test
+        dispatch_test = Dispatch.find(order='123-123')
+
+        self.assertEqual(dispatch_test.country_source, country_source)
+        self.assertEqual(dispatch_test.country_target, country_target)
+
         dispatch.delete()
 
 
