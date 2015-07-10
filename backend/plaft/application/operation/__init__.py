457214078fa4d13bcc613b318f0a54fa3bd97174
Javier Huaman <javier.adama@gmail.com>
2015-07-10 15:20:39 -0500

    Casos de uso modificados, `accept` y `accept_multiple`.

--- backend/plaft/application/operation/__init__.py
+++ backend/plaft/application/operation/__init__.py
@@ -9,9 +9,12 @@ def accept(dispatch):
     TODO: Falta lanzar excepción cuando ya está aceptado.
 
     """
+    datastore = dispatch.customs_agency.datastore
+    counter = datastore.last_counter_operation + 1
     operation = Operation(dispatches_key=[dispatch.key],
                           customs_agency_key=dispatch.customs_agency_key,
-                          customer_key=dispatch.customer_key)
+                          customer_key=dispatch.customer_key,
+                          counter=counter)
     operation.store()
 
     dispatch.operation_key = operation.key
@@ -43,11 +46,13 @@ def accept_multiple(customs_agency):
         if pendings_customer:
             amount = sum(float(d.amount) for d in pendings_customer)
             if amount >= 50000:
+                counter = datastore.last_counter_operation + 1
                 operation = Operation(dispatches_key=[d.key
                                                       for d
                                                       in pendings_customer],
                                       customs_agency_key=customs_agency.key,
-                                      customer_key=customer_key)
+                                      customer_key=customer_key,
+                                      counter=counter)
                 operation.store()
 
                 for dispatch in pendings_customer:
