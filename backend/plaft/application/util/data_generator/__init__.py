2732f0170b32e7df6f62e18cb27a04ceb643b08d
cristHian Gz. (gcca) <gcca@gcca.tk>
2015-07-20 17:35:44 -0500

    Application: minimal (default) datastore generator.

---
+++ backend/plaft/application/util/data_generator/__init__.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""
+.. module:: plaft.application.util
+   :synopsis: Provides minial data to work.
+
+.. moduleauthor:: Gonzales Castillo, Cristhian A. <gcca@gcca.tk>
+
+
+"""
+
+def create_alerts():
+    """Main alerts."""
+
+
+# vim: et:ts=4:sw=4
